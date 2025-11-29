cb14f5d97ada33e241bb821cd0dd01229f5eda47
Jhonatan Parada <jhonatanparada499@gmail.com>
2025-11-29 12:57:05 -0500

    feat: add private fields and constructor

--- src/main/java/com/school/app/model/Student.java
+++ src/main/java/com/school/app/model/Student.java
@@ -1,5 +1,20 @@
 package com.school.app.model;
 
+import java.util.List;
+
 public class Student {
   private String id;
+  private String name;
+  private String major;
+
+  private List<ClassSession> enrolledClasses;
+
+  public Student(String theId, String theName,
+      String theMajor,
+      List<ClassSession> theEnrolledClasses) {
+    id = theId;
+    name = theName;
+    major = theMajor;
+    enrolledClasses = theEnrolledClasses;
+  }
 }
