c852a1fab2cc1818aab111c07a20c1fe719d6404
Jhonatan Parada <jhonatanparada499@gmail.com>
2025-11-29 16:34:44 -0500

    feat: add defintion for csv dataloader

--- src/main/java/com/school/app/service/InstructorService.java
+++ src/main/java/com/school/app/service/InstructorService.java
@@ -1,5 +1,51 @@
 package com.school.app.service;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.List;
+
+import com.school.app.model.Instructor;
+
+// inspiration: 
+// https://medium.com/@AlexanderObregon/javas-scanner-usedelimiter-method-explained-8e09e2baf831
+
 public class InstructorService {
+
+  public static HashMap<String, Instructor> load() {
+    HashMap<String, Instructor> instructors = new HashMap<>();
+
+    String homeDir = "/home/jhonatan/";
+    String fileDir = homeDir + "Projects/Github/jhonatanparada499/" +
+        "school-registration-system/data/";
+    String filePath = fileDir + "instructor.csv";
+
+    // try-resource closes file automatically
+    try (Scanner scanner = new Scanner(new File(filePath))) {
+      scanner.useDelimiter(",|\\n"); // match: comma(,) or new line(\n)
+
+      while (scanner.hasNext()) {
+        String idField = scanner.next();
+        String nameField = scanner.next();
+        String qualifiedCoursesField = scanner.next();
+
+        // split field by the pipe character
+        String[] qualifiedCoursesArray = qualifiedCoursesField.split("\\|");
+        List<String> qualifiedCourses = new ArrayList<>(
+            Arrays.asList(qualifiedCoursesArray));
+
+        Instructor instructor = new Instructor(
+            idField, nameField, qualifiedCourses);
+        instructors.put(idField, instructor);
+      }
+
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+
+    return instructors;
+  }
 
 }
