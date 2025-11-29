2d15c83a14666535599c4f1e89abeb426a6ab01e
Jhonatan Parada <jhonatanparada499@gmail.com>
2025-11-29 13:36:34 -0500

    feat: add constructor that accepts Course parameter

--- src/main/java/com/school/app/model/Course.java
+++ src/main/java/com/school/app/model/Course.java
@@ -3,6 +3,10 @@ package com.school.app.model;
 public class Course {
   // Add private variables
 
+  public Course(Course otherCourse) {
+
+  }
+
   // Add getCredits definition
   public int getCredits() {
     return 0;
