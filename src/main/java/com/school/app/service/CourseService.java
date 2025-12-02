e96f84863076ce8aa62381abbb028c57ee7d3842
Jhonatan Parada <jhonatanparada499@gmail.com>
2025-12-02 17:02:24 -0500

    feat: add CoursesService definition based on InstructorService

--- src/main/java/com/school/app/service/CourseService.java
+++ src/main/java/com/school/app/service/CourseService.java
@@ -1,5 +1,41 @@
 package com.school.app.service;
 
-public class CourseService {
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
+import com.school.app.model.Course;
+
+public class CourseService {
+  public static Map<String, Course> load() {
+    Map<String, Course> courses = new HashMap<>();
+
+    String homeDir = "/home/jhonatan/";
+    String fileDir = homeDir +
+        "Projects/Github/jhonatanparada499/" +
+        "school-registration-system/data/";
+    String fileName = "Course.csv";
+    String filePath = fileDir + fileName;
+
+    // try-resource closes file automatically
+    try (Scanner scanner = new Scanner(new File(filePath))) {
+      scanner.useDelimiter(",|\\n"); // match: comma(,) or new line(\n)
+
+      while (scanner.hasNext()) {
+        String idField = scanner.next();
+        String nameField = scanner.next();
+        int creditsField = scanner.nextInt();
+
+        Course course = new Course(idField,
+            nameField, creditsField);
+        courses.put(idField, course);
+      }
+
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+
+    return courses;
+  }
 }
