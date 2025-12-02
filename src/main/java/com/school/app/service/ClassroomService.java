4327188389d7df7c750cb6abb028e0a4d30c83c2
Jhonatan Parada <jhonatanparada499@gmail.com>
2025-12-02 17:43:06 -0500

    feat: add classroom service method to parse data

--- src/main/java/com/school/app/service/ClassroomService.java
+++ src/main/java/com/school/app/service/ClassroomService.java
@@ -1,5 +1,41 @@
 package com.school.app.service;
 
-public class ClassroomService {
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
+import com.school.app.model.Classroom;
+
+public class ClassroomService {
+  public static Map<String, Classroom> load() {
+    Map<String, Classroom> classrooms = new HashMap<>();
+
+    String homeDir = "/home/jhonatan/";
+    String fileDir = homeDir +
+        "Projects/Github/jhonatanparada499/" +
+        "school-registration-system/data/";
+    String fileName = "Classroom.csv";
+    String filePath = fileDir + fileName;
+
+    // try-resource closes file automatically
+    try (Scanner scanner = new Scanner(new File(filePath))) {
+      scanner.useDelimiter(",|\\n"); // match: comma(,) or new line(\n)
+
+      while (scanner.hasNext()) {
+        String idField = scanner.next();
+        String nameField = scanner.next();
+        int creditsField = scanner.nextInt();
+
+        // Classroom course = new Classroom();
+        // classrooms.put(idField, course);
+      }
+
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+
+    return classrooms;
+  }
+}
 }
