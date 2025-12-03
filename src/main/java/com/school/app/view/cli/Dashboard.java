bebeebf1b16e50a61e0bd8d993e086e182d43fee
Jhonatan Parada <jhonatanparada499@gmail.com>
2025-12-02 19:51:05 -0500

    feat: announce that work is still in progress in Dashboard

--- src/main/java/com/school/app/view/cli/Dashboard.java
+++ src/main/java/com/school/app/view/cli/Dashboard.java
@@ -2,6 +2,7 @@ package com.school.app.view.cli;
 
 public class Dashboard {
   public static void display() {
-    //
+    System.out.println("Still working in this area");
+    System.exit(0);
   }
 }
