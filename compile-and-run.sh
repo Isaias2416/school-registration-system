b38752a7cd90717f5da0bf8b6f4acac1a502d468
Jhonatan Parada <jhonatanparada499@gmail.com>
2025-12-04 11:21:15 -0500

    docs: specify that script is meant to run the CLI version of the project

--- compile-and-run.sh
+++ compile-and-run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Only use this script to run terminal version of this project.
+# Use mvn clean install, then mvn javafx:run
+
 find ./src/main/ -type f -name "*.java" >sources.txt
 
 javac -d ./output/ @sources.txt
