d3bb3ea0f12681a3a4a94c86890c4eb14215dc04
Jhonatan Parada <jhonatanparada499@gmail.com>
2025-12-06 17:35:12 -0500

    feat: implement loop and error handling for form

--- src/main/java/com/school/app/view/cli/StudentRegistrator.java
+++ src/main/java/com/school/app/view/cli/StudentRegistrator.java
@@ -11,37 +11,63 @@ import com.school.app.model.Student;
 
 public class StudentRegistrator {
   public static void display() {
-    System.out.print("Enter Student ID: ");
+    boolean completedForm = false;
+    while (!completedForm) {
+      System.out.print("Enter Student ID: ");
 
-    Scanner keyboard = new Scanner(System.in);
-    String studentIdChoice = keyboard.nextLine().trim();
+      Scanner keyboard = new Scanner(System.in);
+      String studentIdChoice = keyboard.nextLine().trim();
 
-    // displays list of available class sections
-    Map<Integer, ClassSession> classSections = ClassSessionService.load();
-    System.out.println(
-        "ID : Course | Instructor | Classroom | Section Number");
-    for (ClassSession classSection : classSections.values()) {
+      Map<String, Student> students = StudentService.load();
+      if (students.get(studentIdChoice) == null) {
+        System.out.println("Invalid Student Id, try again:");
+        continue;
+      }
+
+      // displays list of available class sections
+      Map<Integer, ClassSession> classSections = ClassSessionService.load();
       System.out.println(
-          classSection.getId() +
-              " : " + classSection.getCourse() +
-              " | " + classSection.getInstructor() +
-              " | " + classSection.getClassroom() +
-              " | " + classSection.getSectionNumber());
+          "ID : Course | Instructor | Classroom | Section Number");
+      for (ClassSession classSection : classSections.values()) {
+        System.out.println(
+            classSection.getId() +
+                " : " + classSection.getCourse() +
+                " | " + classSection.getInstructor() +
+                " | " + classSection.getClassroom() +
+                " | " + classSection.getSectionNumber());
+      }
+
+      System.out.print("Enter Class Section ID: ");
+      String classSectionIdChoice = keyboard.nextLine().trim();
+
+      boolean validClassSection = false;
+      while (!validClassSection) {
+
+        ClassSession classSection = classSections.get(
+            Integer.parseInt(classSectionIdChoice));
+
+        if (classSection == null) {
+          System.out.println("Invalid class Section choice, try again");
+          continue;
+        }
+        validClassSection = true;
+
+        // at this point student is not null
+        Student student = students.get(studentIdChoice);
+
+        completedForm = RegistrationService.registerStudent(
+            student,
+            classSection);
+
+        if (completedForm == false) {
+          System.out.println("Try again");
+          continue;
+        }
+
+        completedForm = true;
+        System.out.println("Student has been succesfully registered");
+        Administration.display();
+      }
     }
-
-    System.out.print("Enter Class Section ID: ");
-    String classSectionIdChoice = keyboard.nextLine().trim();
-
-    ClassSession classSection = classSections.get(
-        Integer.parseInt(classSectionIdChoice));
-
-    Map<String, Student> students = StudentService.load();
-    Student student = students.get(studentIdChoice);
-
-    RegistrationService.registerStudent(
-        student,
-        classSection);
-
-    keyboard.close();
   }
 }
