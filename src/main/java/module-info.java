85615541a48914a12a98fd791af8fccb9c83ac9e
Jhonatan Parada <jhonatanparada499@gmail.com>
2025-12-08 13:34:51 -0500

    fix: declare opens statement to work with fxml files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,7 @@ module com.school.app {
   requires transitive javafx.graphics;
 
   // If fxml files are added, they must be declared here as in:
-  // opens com.school.app.model to javafx.fxml;
+  opens com.school.app.controller to javafx.fxml;
 
   opens com.school.app to javafx.graphics;
 
