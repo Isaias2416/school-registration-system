3a2b4d3a5be5a9f9afe73c28f3bd3d99bb1834ff
Jhonatan Parada <jhonatanparada499@gmail.com>
2025-12-10 12:27:16 -0500

    feat: initialize the student tab in main controller

--- src/main/java/com/school/app/controller/MainSceneController.java
+++ src/main/java/com/school/app/controller/MainSceneController.java
@@ -8,20 +8,26 @@ import javafx.fxml.FXMLLoader;
 public class MainSceneController {
   @FXML
   private Tab tabAdministration;
+
   @FXML
   private Tab tabDashboard;
+
+  @FXML
+  private Tab tabStudent;
 
   public void initialize() {
     try {
       Parent administrationView = FXMLLoader
           .load(getClass().getResource("/com/school/app/view/administrationScene.fxml"));
       Parent dashboardView = FXMLLoader.load(getClass().getResource("/com/school/app/view/dashboardScene.fxml"));
+      Parent studentView = FXMLLoader.load(getClass().getResource("/com/school/app/view/studentScene.fxml"));
 
       tabAdministration.setContent(administrationView);
       tabDashboard.setContent(dashboardView);
+      tabStudent.setContent(studentView);
 
     } catch (Exception e) {
-      System.out.println("Error from mainScne");
+      System.out.println("Error from mainScene controller.");
       System.out.println(e);
     }
   }
